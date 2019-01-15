b70e54c17e25f83cda9cdd5c2d84c36f47a453bb
vitya7 <31449667+vitya7@users.noreply.github.com>
2019-01-15 18:05:54 +0400

    add main_table

--- include/main_table.hpp
+++ include/main_table.hpp
@@ -2,20 +2,31 @@
 
 #include "copy_overload_table.hpp"
 
+#include "copy_range.hpp"
+
+#include <tuple>
+
 namespace pline
 {
+
+
     class main_table : public copy_overload_table
     {
-    private:
+    public :
 
-            main_table ();
-
-            main_table (main_table const&) = default;
-            main_table (main_table &&) = default;
-
-    public:
+        using iterator_value_types = std::tuple < char, int, float, double, long double >;
 
         static main_table const& get ();
+
+    private:
+
+        main_table ();
+
+        main_table (main_table const&) = default;
+        main_table (main_table &&) = default;
+
+        template <class ItTupleInput, class ItTupleOutput, size_t I1 = 0, size_t I2 = 0>
+        void insert_overloads ();
 
     };
 
---
+++ include/overload_traits.hpp
@@ -0,0 +1,83 @@
+#pragma once
+
+#include <tuple>
+
+namespace pline
+{
+    namespace traits
+    {
+        //////////////////////////////////////////////////////////////
+        template <class> struct add_ptr {};
+
+        template <class...T>
+        struct add_ptr < std::tuple <T...> >
+        {
+            using type = std::tuple <T*... >;
+        };
+
+        template <class T>
+        using add_ptr_t = typename add_ptr <T> :: type;
+
+        //////////////////////////////////////////////////////////////
+        template <class> struct add_const_ptr {};
+
+        template <class...T>
+        struct add_const_ptr < std::tuple <T...> >
+        {
+            using type = std::tuple <T const*... >;
+        };
+
+        template <class T>
+        using add_const_ptr_t = typename add_const_ptr <T> :: type;
+
+        //////////////////////////////////////////////////////////////
+        template <class T, template <class> class ... C>
+        struct get_containers_iterators
+        {
+            using type = std::tuple < typename C<T> :: iterator ... >;
+        };
+
+        template <class T, template <class> class ... C>
+        using get_containers_iterators_t = typename get_containers_iterators <T, C...> :: type;
+
+        template <template <class> class ... C, class ... T>
+        struct get_containers_iterators < std::tuple <T...>, C... >
+        {
+            using type = decltype
+                        (	std::tuple_cat
+                            (
+                                std::declval < get_containers_iterators_t <T, C...> > () ...
+                            )
+                        );
+        };
+
+
+        //////////////////////////////////////////////////////////////
+        template <class T, template <class> class ... C>
+        struct get_containers_const_iterators
+        {
+            using type = std::tuple < typename C<T> :: const_iterator ... >;
+        };
+
+        template <class T, template <class> class ... C>
+        using get_containers_const_iterators_t = typename get_containers_const_iterators <T, C...> :: type;
+
+        template <template <class> class ... C, class ... T>
+        struct get_containers_const_iterators < std::tuple <T...>, C... >
+        {
+            using type = decltype
+                        (	std::tuple_cat
+                            (
+                                std::declval < get_containers_const_iterators_t <T, C...> > () ...
+                            )
+                        );
+        };
+
+
+        //////////////////////////////////////////////////////////////
+        template <class ... T>
+        using tuple_cat_t = decltype ( std::tuple_cat ( std::declval <T> ()...) );
+
+        //////////////////////////////////////////////////////////////
+    }
+}
