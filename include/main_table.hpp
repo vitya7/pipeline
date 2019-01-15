16c4132513346b4468adaed682dac9b7a57953ff
vitya7 <31449667+vitya7@users.noreply.github.com>
2019-01-15 18:12:18 +0400

    Revert "main_table initialization"
    This reverts commit b70e54c17e25f83cda9cdd5c2d84c36f47a453bb.

--- include/main_table.hpp
+++ include/main_table.hpp
@@ -2,31 +2,20 @@
 
 #include "copy_overload_table.hpp"
 
-#include "copy_range.hpp"
-
-#include <tuple>
-
 namespace pline
 {
-
-
     class main_table : public copy_overload_table
     {
-    public :
-
-        using iterator_value_types = std::tuple < char, int, float, double, long double >;
-
-        static main_table const& get ();
-
     private:
 
-        main_table ();
+            main_table ();
 
-        main_table (main_table const&) = default;
-        main_table (main_table &&) = default;
+            main_table (main_table const&) = default;
+            main_table (main_table &&) = default;
 
-        template <class ItTupleInput, class ItTupleOutput, size_t I1 = 0, size_t I2 = 0>
-        void insert_overloads ();
+    public:
+
+        static main_table const& get ();
 
     };
 
--- src/main_table.cpp
+++ src/main_table.cpp
@@ -2,66 +2,12 @@
 
 #include "copy_range.hpp"
 
-#include "overload_traits.hpp"
-
-#include <iostream>
-#include <vector>
-
 namespace pline
 {
-
-//    template <class ... T>
-//    void w ()
-//    {
-//        std::cerr << __PRETTY_FUNCTION__ << "\n";
-//    }
-
-    template <class ItTupleInput, class ItTupleOutput, size_t I1, size_t I2>
-    void
-    main_table::
-    insert_overloads ()
-    {
-        if constexpr ( I1 == std::tuple_size_v < ItTupleInput > ) {
-            return;
-        }
-        else
-        if constexpr ( I2 == std::tuple_size_v < ItTupleOutput >) {
-            return;
-        }
-        else
-        {
-            using It1 = std::tuple_element_t <I1, ItTupleInput>;
-            using It2 = std::tuple_element_t <I2, ItTupleOutput>;
-
-//            w < It1, It2 > ();
-
-            insert_overload ( make_key <It1, It2> (), copy_range <It1, It2> :: copy );
-
-            insert_overloads < ItTupleInput, ItTupleOutput, I1, I2+1 > ();
-
-            if constexpr (I2 == 0)
-            {
-                insert_overloads < ItTupleInput, ItTupleOutput, I1+1, I2 > ();
-            }
-        }
-    }
-
     main_table::
     main_table()
     {
-        using in_t = traits::tuple_cat_t
-                    <	traits::add_ptr_t                           < iterator_value_types >
-                    ,	traits::add_const_ptr_t                     < iterator_value_types >
-                    ,	traits::get_containers_iterators_t 		    < iterator_value_types, std::vector >
-                    ,	traits::get_containers_const_iterators_t    < iterator_value_types, std::vector >
-                    >;
-
-        using out_t = traits::tuple_cat_t
-                    <	traits::add_ptr_t                           < iterator_value_types >
-                    ,	traits::get_containers_iterators_t 		    < iterator_value_types, std::vector >
-                    >;
-
-        insert_overloads < in_t, out_t > ();
+        insert_overload( make_key <int*, int*> (), copy_range <int*, int*> :: copy );
     }
 
     main_table const&
